072aa8696e7e0f93b94c160cb31e8257455ea15e
Choijake <shameless8@naver.com>
2023-11-15 00:49:10 +0900

    feat : 입력에 대한 검증 기능 추가

---
+++ src/main/java/christmas/view/input/InputHandler.java
@@ -0,0 +1,54 @@
+package christmas.view.input;
+
+import static christmas.view.constant.printConstant.ASK_MENU_AND_QUANTITY_MESSAGE;
+import static christmas.view.constant.printConstant.ASK_VISIT_DATE_MESSAGE;
+
+import christmas.domain.Day;
+import christmas.domain.Order;
+import christmas.exception.OrderException;
+import christmas.view.input.Parser;
+import christmas.view.output.OutputWriter;
+import christmas.view.input.InputReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class InputHandler {
+    public static Day requestDay(){
+        OutputWriter.printMessage(ASK_VISIT_DATE_MESSAGE);
+        return readDateInformation();
+    }
+
+    public static List<Order> requestOrder(){
+        OutputWriter.printMessage(ASK_MENU_AND_QUANTITY_MESSAGE);
+        return readMenuAndQuantity();
+    }
+
+    private static Day readDateInformation(){
+        try {
+            final String dateInput = InputReader.readLine();
+            final int date = Parser.parseStringToInt(dateInput);
+            return Day.of(date);
+        } catch (OrderException exception) {
+            OutputWriter.println(exception.getMessage());
+            return readDateInformation();
+        }
+    }
+
+    private static List<Order> readMenuAndQuantity(){
+        try {
+            final String menuAndQuantity = InputReader.readLine();
+            List<String> menuAndQuantityList = Parser.splitByDelimiter(menuAndQuantity);
+            List<Order> orders = new ArrayList<>();
+
+            menuAndQuantityList.stream()
+                    .map(m -> m.split("-"))
+                    .forEach(part -> orders.add(new Order(part[0], Integer.parseInt(part[1]))));
+
+            return orders;
+        } catch (OrderException exception) {
+            OutputWriter.println(exception.getMessage());
+            return readMenuAndQuantity();
+        }
+    }
+
+}

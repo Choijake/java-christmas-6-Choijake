9a702dbc09ffeae550c9b760ada1eef2037b9488
Choijake <shameless8@naver.com>
2023-11-15 00:56:39 +0900

    fix : 주말 할인, 평일 할인 버그 수정

--- src/main/java/christmas/domain/Discount.java
+++ src/main/java/christmas/domain/Discount.java
@@ -1,5 +1,7 @@
 package christmas.domain;
 
+import christmas.view.constant.printConstant;
+
 public class Discount {
    private static final int WEEK_SALE_DISCOUNT_AMOUNT = 2023;
    public static final int STAR_DAY_DISCOUNT_AMOUNT = 1000;
@@ -24,24 +26,35 @@ public class Discount {
                + getWeekEndDiscountAmount();
    }
 
-   public int getDDayDiscountAmount(){
-       return orderSheet.getDDayDiscountAmount();
+   //크리스마스 디데이 할인
+   public boolean isBeforeDDay(){
+       return orderSheet.isBeforeDDay();
    }
 
+   public int getDDayDiscountAmount(){
+       if(isBeforeDDay()){
+           return orderSheet.getDDayDiscountAmount();
+       }
+       return 0;
+   }
+
+   //평일 할인
    public int getWeekDayDiscountAmount(){
-       if(orderSheet.isWeekDay()){
+       if(orderSheet.isWeekDay() && orderSheet.getDesertCount() > 0){
            return orderSheet.getDesertCount() * WEEK_SALE_DISCOUNT_AMOUNT;
        }
        return 0;
    }
 
+   //주말 할인
    public int getWeekEndDiscountAmount(){
-       if(orderSheet.isWeekEnd()){
+       if(orderSheet.isWeekEnd() && orderSheet.getMainCount() > 0){
            return orderSheet.getMainCount() * WEEK_SALE_DISCOUNT_AMOUNT;
        }
        return 0;
    }
 
+   //특별 할인
    public int getStarDayDiscountAmount(){
        if(orderSheet.hasStar()){
            return STAR_DAY_DISCOUNT_AMOUNT;
@@ -49,6 +62,7 @@ public class Discount {
        return 0;
    }
 
+   //증정 이벤트
    public int getGiftPromotionDiscountAmount(){
        if(orderSheet.getTotalPrice() >= GIFT_PROMOTION_QUALIFIER_AMOUNT){
            return GIFT_PROMOTION_DISCOUNT_AMOUNT;
@@ -61,26 +75,26 @@ public class Discount {
         StringBuilder sb = new StringBuilder();
 
         if(orderSheet.isBeforeDDay()){
-            sb.append("크리스마스 디데이 할인: -").append(getDDayDiscountAmount()).append("원\n");
+            sb.append(printConstant.SHOW_D_DAY_DISCOUNT.getMessage()).append(getDDayDiscountAmount()).append(printConstant.WON.getMessage());
         }
 
         if(orderSheet.isWeekDay() && orderSheet.getDesertCount() > 0){
-            sb.append("평일 할인: -").append(getWeekDayDiscountAmount()).append("원\n");
+            sb.append(printConstant.SHOW_WEEK_DAY_DISCOUNT.getMessage()).append(getWeekDayDiscountAmount()).append(printConstant.WON.getMessage());
         }
 
         if(orderSheet.isWeekEnd() && orderSheet.getMainCount() > 0){
-            sb.append("주말 할인: -").append(getWeekEndDiscountAmount()).append("원\n");
+            sb.append(printConstant.SHOW_WEEK_END_DISCOUNT.getMessage()).append(getWeekEndDiscountAmount()).append(printConstant.WON.getMessage());
         }
 
         if(orderSheet.hasStar()){
-            sb.append("특별 할인: -").append(getStarDayDiscountAmount()).append("원\n");
+            sb.append(printConstant.SHOW_STAR_DAY_DISCOUNT.getMessage()).append(getStarDayDiscountAmount()).append(printConstant.WON.getMessage());
         }
 
         if(orderSheet.getTotalPrice() >= GIFT_PROMOTION_QUALIFIER_AMOUNT){
-            sb.append("증정 이벤트: -").append(getGiftPromotionDiscountAmount()).append("원\n");
+            sb.append(printConstant.SHOW_GIFT_PROMOTION_DISCOUNT.getMessage()).append(getGiftPromotionDiscountAmount()).append(printConstant.WON.getMessage());
         }
 
-        sb.append("없음");
+        sb.append(printConstant.SHOW_NO_DISCOUNT.getMessage());
 
         return sb.toString();
     }

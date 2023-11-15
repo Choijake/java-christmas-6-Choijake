1b9862bce0b791c957c306a360c290f30c7d2c41
Choijake <shameless8@naver.com>
2023-11-15 12:46:45 +0900

    feat : 입력과 검증을 분리

---
+++ src/main/java/christmas/view/input/InputView.java
@@ -0,0 +1,20 @@
+package christmas.view.input;
+
+import static christmas.view.constant.printConstant.ASK_VISIT_DATE_MESSAGE;
+import static christmas.view.constant.printConstant.WELCOME_MESSAGE;
+
+import christmas.domain.Day;
+import christmas.domain.Order;
+import christmas.view.output.OutputWriter;
+import java.util.List;
+
+public class InputView {
+    public static Day inputVisitDate(){
+        OutputWriter.printMessage(WELCOME_MESSAGE);
+        return InputHandler.requestDay();
+    }
+
+    public static List<Order> inputOrder(){
+        return InputHandler.requestOrder();
+    }
+}

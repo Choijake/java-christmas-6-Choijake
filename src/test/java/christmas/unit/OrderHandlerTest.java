2c12477a20de1dbd0cd469c2853d75d89e978d7b
Choijake <shameless8@naver.com>
2023-11-15 21:10:19 +0900

    Test : 주문 기능에 대한 테스트 코드 추가

--- src/test/java/christmas/unit/OrderHandlerTest.java
+++ src/test/java/christmas/unit/OrderHandlerTest.java
@@ -3,19 +3,20 @@ package christmas.unit;
 import christmas.domain.Customer;
 import christmas.domain.Day;
 import christmas.domain.Order;
+import christmas.domain.OrderSheet;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
 import java.util.List;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class OrderHandlerTest {
     @Test
     @DisplayName("주문 하는 기능에 대한 테스트")
     void createCustomerWithOrder() {
-        // Arrange
         Day testDay = Day.of(12);
         List<Order> testOrders = Arrays.asList(
                 new Order("바비큐립", 2),
@@ -23,11 +24,22 @@ public class OrderHandlerTest {
                 new Order("제로콜라", 1)
         );
 
-        // Act
         Customer customer = Customer.order(testDay, testOrders);
 
-        // Assert
         assertEquals(testDay, customer.getDay());
         assertEquals(testOrders, customer.getOrders());
+    }
+
+    @DisplayName("writeOrderSheet 메서드 테스트")
+    @Test
+    void testWriteOrderSheet() {
+        Day day = Day.of(3);
+        List<Order> orders = List.of(
+                new Order("초코케이크", 2),
+                new Order("샴페인", 3)
+        );
+        Customer customer = Customer.order(day,orders);
+
+        customer.writeOrderSheet();
     }
 }
